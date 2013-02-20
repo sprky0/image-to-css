5e2896a5760131924ad51267d5586b2e9d7ec735
Avery Brooks <avery@HAL.local>
2013-02-20 11:28:10 -0500

    part of an upload form to "generate" this garbage

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+	<h1>Convert your image to a very large and cumbersome HTML file with inline CSS.</h1>
+
+	<form method="post" enctype="multipart/form-data">
+		<!--
+
+			// options
+			// inline CSS, CSS in STYLE tag
+
+		-->
+		<label for="file">Select a file:</label>
+		<input type="file" name="file" id="file"><br />
+		<input type="submit" name="submit" value="Submit" />
+	</form>
+
+</body>	
+</html>
