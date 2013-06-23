2bd5749b165323e5b130d92e591961c4b318e611
Avery Brooks <avery@Averys-MacBook-Pro.local>
2013-06-23 14:49:41 -0400

    shorter tags, remove alpha

--- image_to_css.php
+++ image_to_css.php
@@ -10,6 +10,14 @@ class ImageToCSS {
 	protected $buffer = "";
 	protected $html = true;
 
+	protected $pixelElement = "b";
+
+	protected $width = 640;
+	protected $height = 480;
+
+	protected $colors = array();
+	protected $positions = array();
+
 	public function setWrap($wrap = false) {
 		$this->html = $wrap;
 	}
@@ -28,22 +36,44 @@ class ImageToCSS {
 			case "string": $i = imagecreatefromstring($image); break;
 		}
 
-		$width = imagesx($i);
-		$height = imagesy($i);
-	
-		for($x = 0; $x < $width; $x++) {
-			for($y = 0; $y < $height; $y++) {
+		$this->width = imagesx($i);
+		$this->height = imagesy($i);
+
+		// first loop populates the pallete
+		// the reason for this is that we may try to apply compression
+		// can replace the image operation on the second loop with direct access to an array with the meta from this loop later
+		/*
+		for($x = 0; $x < $this->width; $x++) {
+			for($y = 0; $y < $this->height; $y++) {
+				$colors = imagecolorsforindex($i,$rgb);
+				$rgb = imagecolorat($i,$x,$y);
+				$colors = imagecolorsforindex($i,$rgb);
+				$this->color($colors['red'],$colors['green'],$colors['blue']);
+			}
+		}
+		*/
+
+		for($x = 0; $x < $this->width; $x++) {
+			for($y = 0; $y < $this->height; $y++) {
 				$rgb = imagecolorat($i,$x,$y);
 				$colors = imagecolorsforindex($i,$rgb);
 				$this->pixel($x,$y,$colors['red'],$colors['green'],$colors['blue'],1 - ($colors['alpha'] / 127));
 			}
 		}
 
+		$instance = "r";
+
+		$CSS = ".$instance{width:{$this->width}px;height:{$this->height}px;position:relative;}";
+		$CSS .= ".a{position:absolute}";
+		$CSS .= ".p{width:1px;height:1px;display:block;}";
+		$CSS .= implode($this->colors,"");
+		$CSS .= implode($this->positions,"");
+
 		if ($this->html === true)
-			$result = "<html><body><div style=\"width:{$width}px;height:{$height}px;position:relative;\">{$this->buffer}</div></body></html>";
+			$result = "<html><body><style>{$CSS}</style><div class=\"$instance\">{$this->buffer}</div></body></html>";
 		else
-			$result = "<div style=\"width:{$width}px;height:{$height}px;position:relative;\">{$this->buffer}</div>";
-			
+			$result = "<style>{$CSS}</style><div class=\"$instance\">{$this->buffer}</div></div>";
+
 		if (!is_null($output_file))
 			return file_put_contents($output_file,$result);
 		else
@@ -51,16 +81,42 @@ class ImageToCSS {
 
 	}
 
-	private function block($x,$y,$w,$h,$r,$g,$b,$a=1) {
-
-		$this->buffer .= "<span style=\"position:absolute;background-color:rgba($r,$g,$b,$a);top:{$y}px;left:{$x}px;\"></span>";
-
-	}
-			
 	private function pixel($x,$y,$r,$g,$b,$a=1) {
 
-		$this->buffer .= "<span style=\"position:absolute;background-color:rgba($r,$g,$b,$a);top:{$y}px;left:{$x}px;width:1px;height:1px;\"></span>";
+		// skip transparency for now
+		if ($a < 1)
+			return;
+
+		// $r$g$b$a classname is long, could shorten it
+
+		if (!isset($this->colors["c$r$g$b$a"])) {
+			// $this->colors["c$r$g$b$a"] = ".cc$r$g$b$a{background-color:rgba($r,$g,$b,$a)}";
+			$hex = $this->rgbToHex($r,$g,$b);
+			$this->colors["c$r$g$b$a"] = ".c$r$g$b$a{background-color:#{$hex}}";
+		}
+
+		if (!isset($this->positions["x$x"]))
+			$this->positions["x$x"] = ".x$x{left:{$x}px}";
+
+		if (!isset($this->positions["y$y"]))
+				$this->positions["y$y"] = ".y$y{top:{$y}px}";
+
+		$classes = "a x$x y$y c$r$g$b$a p";
+
+		$this->buffer .= "<{$this->pixelElement} class=\"{$classes}\"></{$this->pixelElement}>";
 
 	}
-	
+
+	/*
+	private function color($r,$g,$b) {
+		$rgb = imagecolorat($i,$x,$y);
+		$colors = imagecolorsforindex($i,$rgb);
+		$this->pixel($x,$y,$colors['red'],$colors['green'],$colors['blue'],1 - ($colors['alpha'] / 127));	
+	}
+	*/
+
+	private function rgbToHex($r,$g,$b) {
+		return str_pad(dechex($r), 2, "0", STR_PAD_LEFT) . str_pad(dechex($g), 2, "0", STR_PAD_LEFT) . str_pad(dechex($b), 2, "0", STR_PAD_LEFT);
+	}
+
 }
